6d21df6c69ad515d5d0c79473658aa85aaedb665
Diogo Junior de Souza <diogo.junior@grad.ufsc.br>
2018-05-24 14:30:25 -0300

    Test with object made in Blender

--- src/rudolph/window.cpp
+++ src/rudolph/window.cpp
@@ -349,7 +349,7 @@ void MainWindow::show() {
 
     renderer.add_object(Object3D(xpoints, xedges, std::vector<Face>()));
     */
-    renderer.load_obj("objs/subzero.obj");
+    renderer.load_obj("cubinho.obj");
 
     update_list();
 }
